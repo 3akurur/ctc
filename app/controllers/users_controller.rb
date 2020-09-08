0e95b0a9246fc0e5ebba8adc7212f9d4b8220676
rurukasan <ruka20020212@icloud.com>
2020-09-09 01:19:12 +0900

    logout_function_implementation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,6 +44,7 @@ class UsersController < ApplicationController
   def login
     @user = User.find_by(name: params[:name], password: params[:password])
     if @user
+      session[:user_id] = @user.id
       flash[:notice] = "ログインしました"
       redirect_to("/posts/index")
     else
@@ -52,5 +53,11 @@ class UsersController < ApplicationController
       @password = params[:password]
       render("users/login_form")
     end
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:notice] = "ログアウトしました"
+    redirect_to("/login")
   end
 end
