a486a49fcdba8e6f741019b7722a612ed7f5ffd5
rurukasan <ruka20020212@icloud.com>
2020-09-09 02:50:01 +0900

    login_function_implementation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+  before_action :authenticate_user, {only: [:index, :show, :edit, :update]}
+  before_action :forbid_login_user, {only: [:new, :create, :login_form, :login]}
+  before_action :ensure_correct_user, {only: [:edit, :update]}
+
   def index
     @users = User.all.order(created_at: :desc)
   end
@@ -12,8 +17,14 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(name: params[:name], term: params[:term], profile: params[:profile])
+    @user = User.new(
+      name: params[:name], 
+      term: params[:term],
+      profile: params[:profile],
+      password: params[:password]
+    )
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "ユーザー登録が完了しました"
       redirect_to("/users/#{@user.id}")
     else
@@ -60,4 +71,12 @@ class UsersController < ApplicationController
     flash[:notice] = "ログアウトしました"
     redirect_to("/login")
   end
+
+  def ensure_correct_user
+    if @current_user.id != params[:id].to_i
+      flash[:notice] = "権限がありません"
+      redirect_to("/posts/index")
+    end
+  end
+
 end
